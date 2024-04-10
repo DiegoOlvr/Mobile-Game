7d11de9493eead7fbe3cd463911aa607203603ca
Diego Oliveira Ramos <oliver.diegoramos@gmail.com>
2024-04-10 12:35:41 -0300

    Initial commit

---
+++ Assets/Scripts/Jogador.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jogador : MonoBehaviour
+{
+    // VARIÁVEIS
+    public Transform limite_direita, limite_esquerda;
+    public float velocidade; 
+    public bool direita;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
