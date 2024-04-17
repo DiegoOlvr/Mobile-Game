dbaee08706f76ae7e2f2642b676fe4e5d70e4f81
Diego Oliveira Ramos <oliver.diegoramos@gmail.com>
2024-04-17 18:17:30 -0300

    Fazendo movimentação do player

--- Assets/Scripts/Jogador.cs
+++ Assets/Scripts/Jogador.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +8,10 @@ public class Jogador : MonoBehaviour
     // VARIÁVEIS
     public Transform limite_direita, limite_esquerda;
     public float velocidade; 
-    public bool direita;
+    public bool direita, mouse, barra_espaco; // TRUE OR FALSE
+    public Rigidbody2D fisica;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +21,42 @@ public class Jogador : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        mover();
+        clicar();
+    }
+
+    private void clicar()
+    {
+        // IDENTIFICAR O CLICK DO MOUSE
+        mouse = Input.GetMouseButtonDown(0);
+        // IDENTIFICAR O APERTAR DA BARRA DE ESPAÇO
+        barra_espaco = Input.GetKeyDown("space");
+
+        if (mouse == true || barra_espaco == true)
+        {
+            // ALTERNAR VALOR DA VARIÁVEL BOLEANA
+            direita = !direita;
+        }
+
+    }
+
+    private void mover()
+    {
+        if(direita == true)
+        {
+            fisica.velocity = Vector2.right * velocidade * Time.deltaTime;
+        }
+        else
+        {
+            fisica.velocity = Vector2.left * velocidade * Time.deltaTime;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Limite"))
+        {
+            direita = !direita;
+        }
     }
 }
